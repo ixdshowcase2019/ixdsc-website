1f61253f28219e98f35ab759b7881f9671f90a6a
Hubert <hubhcsi@gmail.com>
2019-03-27 23:18:36 -0400

    LOGOS. EVERYWHERE

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="nav caps">
-        <nav>
-            <div>
-                IXD Showcase 2019
-                <img v-if="route=='home'" src="star.svg">
+    <div class="header">
+        <div class="slogo">
+            <div class="inner">
+                <img src="logo.png" width="80px">
             </div>
-            <div>
-                <nuxt-link class="disabled" to="/home">
-                </nuxt-link>
-            </div>
-        </nav>
+        </div>
+        <div class="nav caps">
+            <nav>
+                <div>
+                    IXD Showcase 2019
+                    <img v-if="route=='home'" src="star.svg">
+                </div>
+                <div>
+                    <nuxt-link class="disabled" to="/home">
+                    </nuxt-link>
+                </div>
+            </nav>
+        </div>
     </div>
 </template>
 
