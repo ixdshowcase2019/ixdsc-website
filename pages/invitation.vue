5bb41d6086ffcdc8f5b4c9ce47f67bde9a864559
Hubert <hubhcsi@gmail.com>
2019-04-04 19:25:25 -0400

    i am done

---
+++ pages/invitation.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="body">
+        <div class="main">
+            <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462">
+                <img src="piece-of-shit.png">
+            </a>
+            <p>
+                To mix and mingle with Sheridan's latest cohort of interaction designers.
+            </p>
+            <p>
+                Whether you're looking to make new connections, build your team, or find fresh takes on trends, our 60+ newly-minted creative professionals will be ready to show you <nuxt-link to="/">their work</nuxt-link>.
+            </p>
+            <p>
+                Stop by and strike up some conversations! <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462">RSVP today</a>.
+            </p>
+            <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462">
+                <img src="Capture.PNG">
+            </a>
+        </div>
+        <div class="footer">
+            IXD Showcase 2019 is sponsored by
+            <div class="logos">
+                <a href="https://www.freshbooks.com/" target=_blank>
+                    <img src="freshbooks.svg">
+                </a>
+                <a href="http://www.ariagency.ca/" target=_blank>
+                    <img src="ariagency.svg">
+                </a>
+                <a href="https://tigroup.ca/" target=_blank>
+                    <img src="t-igroup.svg">
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    .main{
+        width: 600px;
+    }
+    .footer{
+        background: #003767;
+        text-align: center;
+    }
+    .logos{
+        width: 400px;
+        margin: auto;
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
