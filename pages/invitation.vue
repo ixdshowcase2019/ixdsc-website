2edcc406eacb4ebe8e1d232a2d59b5901549616b
Ben Reimer <benreimer9@gmail.com>
2019-04-05 10:41:36 -0400

    variety of small fixes

--- pages/invitation.vue
+++ pages/invitation.vue
@@ -1,8 +1,21 @@
 <template>
     <div class="body">
         <div class="main">
+            <div class="header">
+                <img src="sheridan-fad-logo.png" class="fadLogo"/>
+                <a href="https://www.instagram.com/ixdshowcase2019/" target=_blank>
+                    <img src="insta-orange.png" class="social"/>
+                </a>
+                <a href="https://ixdshowcase.com/" target=_blank>
+                    <img src="invite-link.png" class="social"/>
+                </a>
+            </div>
             <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462">
-                <img src="piece-of-shit.png">
+                <img src="invite-header.png" width="100%" class="inviteHeader">
+            </a>
+            <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462">
+                <img src="youreinvited.png" width="100%" class="youreinvited">
+                <img src="youreinvited-mobile.png" width="100%" class="youreinvited-mobile">
             </a>
             <p>
                 To mix and mingle with Sheridan's latest cohort of interaction designers.
@@ -13,21 +26,30 @@
             <p>
                 Stop by and strike up some conversations! <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462">RSVP today</a>.
             </p>
-            <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462">
-                <img src="Capture.PNG">
+            <div class="details">
+              <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462" target=_blank>
+                  <img src="event-when.png"/>
+              </a>
+              <a href="https://goo.gl/maps/Ja5sFQgRGbS2" target=_blank>
+                  <img src="event-where.png"/>
+              </a>
+            </div>
+            <a href="https://www.eventbrite.ca/e/sheridan-college-ixd-showcase-2019-tickets-59598414462" target=_blank>
+                <img src="rsvpnow.png" width="100%" class="rsvpnow"/>
+                <img src="rsvpnow-mobile.png" width="100%" class="rsvpnow-mobile"/>
             </a>
         </div>
         <div class="footer">
-            IXD Showcase 2019 is sponsored by
+            <p>IXD Showcase 2019 is sponsored by</p>
             <div class="logos">
                 <a href="https://www.freshbooks.com/" target=_blank>
-                    <img src="freshbooks.svg" width="100px">
+                    <img src="freshbooks-white.png">
                 </a>
                 <a href="http://www.ariagency.ca/" target=_blank>
-                    <img src="ariagency.svg" width="100px">
+                    <img src="ariagency-white.png">
                 </a>
                 <a href="https://tigroup.ca/" target=_blank>
-                    <img src="t-igroup.svg" width="100px">
+                    <img src="tigroup-white.png">
                 </a>
             </div>
         </div>
@@ -57,22 +79,90 @@
         margin: auto;
         max-width: 600px;
         width: 100%;
+        padding-bottom: 50px;
+    }
+    .header{
+      position:relative;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      padding: 25px 0px 40px 0px;
+      border-top: 5px solid #003767;
     }
     .footer{
         background: #003767;
         color: white;
         text-align: center;
-        padding: 50px 0;
+        padding: 25px 0 30px 0;
     }
     .logos{
-        padding-top: 25px;
-        width: 400px;
+        padding-top: 10px;
+        width: 360px;
         margin: auto;
         display: flex;
         justify-content: space-between;
         align-items: center;
     }
-    .main img {
-      width: 100%;
+    .logos img{
+      width: 120px;
+    }
+    .fadLogo{
+      width: 90%;
+      max-width:285px;
+      margin-right: auto;
+    }
+    .social{
+      width: 21px;
+      margin: 0 4px;
+    }
+    .inviteHeader{
+      margin-bottom: 55px;
+    }
+    .youreinvited{
+      margin-bottom: 25px;
+      display:none;
+    }
+    .rsvpnow{
+      display:none;
+    }
+    .main p{
+      line-height: 24px;
+      margin-bottom: 20px;
+    }
+    .details img{
+      width:100%;
+    }
+    .footer p{
+      font-style: italic;
+      font-weight: 700;
+      font-size: 12px;
+    }
+    @media(max-width:600px){
+      .logos{
+        flex-direction: column;
+      }
+      .social{
+        display:none;
+      }
+      .fadLogo{
+        width:100%;
+      }
+    }
+    @media(min-width:601px){
+      .youreinvited-mobile{
+        display:none;
+      }
+      .rsvpnow-mobile{
+        display:none;
+      }
+      .youreinvited{
+        display:block;
+      }
+      .rsvpnow{
+        display:block;
+      }
+      .details img{
+        width: 50%;
+      }
     }
 </style>
