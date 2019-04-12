6066d8e19f11a65a4a096ae8000483e8b7f5d079
Ben Reimer <benreimer9@gmail.com>
2019-04-12 14:18:47 -0400

    styling updates

--- components/bio.vue
+++ components/bio.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="modal">
-        <div class="bio">
+
+      <!-- bio for ipad pro and larger   -->
+        <div class="bio largeScreen">
             <img class="close" src="close.svg">
             <div class="left">
                 <img class="profileimage" v-if="g.profile_image" :src="apiurl + g.profile_image.path" width="200px">
@@ -49,6 +51,58 @@
                 <div>
                     {{g.bio_long}}
                 </div>
+            </div>
+        </div>
+
+        <!-- Mobile Version-->
+        <div class="bio mobileScreen">
+            <img class="close" src="close.svg">
+            <div class="left">
+                <img class="profileimage" v-if="g.profile_image" :src="apiurl + g.profile_image.path" width="200px">
+                <img class="profileimage" v-else src="https://dummyimage.com/200x200/000/fff">
+            </div>
+            <div class="right">
+                <div class="s3 display">
+                    {{g.first_name}} {{g.last_name}}
+                </div>
+                <div class="portfolio">
+                    <a :href="helper.linkify(g.portfolio)" target="_blank">{{helper.unlinkify(g.portfolio)}}</a>
+                    <!-- <a :href="helper.linkify(g.portfolio)">{{g.portfolio}}</a> -->
+                </div>
+                <div class="email">
+                    <a :href="'mailto:'+g.email">{{g.email}}</a>
+                </div>
+                <div class="social">
+                    <a v-if="g.twitter" :href="'http://twitter.com/'+g.twitter" target="_blank">
+                        <img src="twitter.png">
+                    </a>
+                    <a v-if="g.linkedin" :href="helper.linkify(g.linkedin)" target="_blank">
+                        <img src="linkedin.png">
+                    </a>
+                    <a v-if="g.instagram" :href="'http://instagram.com/'+g.instagram" target="_blank">
+                        <img src="instagram.png">
+                    </a>
+                    <a v-if="g.behance" :href="helper.linkify(g.behance)" target="_blank">
+                        <img src="behance.png">
+                    </a>
+                    <a v-if="g.dribbble" :href="helper.linkify(g.dribbble)" target="_blank">
+                        <img src="dribbble.png">
+                    </a>
+                    <a v-if="g.github" :href="helper.linkify(g.github)" target="_blank">
+                        <img src="github.png">
+                    </a>
+                    <a v-if="g.medium" :href="helper.linkify(g.medium)" target="_blank">
+                        <img src="medium.png">
+                    </a>
+                    <a v-if="g.other_social" :href="helper.linkify(g.other_social)" target="_blank">
+                        <img src="other_social.png">
+                    </a>
+                </div>
+            </div>
+            <div class="center">
+              <div>
+                  {{g.bio_long}}
+              </div>
             </div>
         </div>
     </div>
